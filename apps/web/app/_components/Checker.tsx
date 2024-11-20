450ce5ce2cbc7ed101ce9fee4537ce75d188fb1b
codersaadi <userid925925@gmail.com>
2024-11-20 22:22:26 +0500

    minor changes , saving state , build is working

--- apps/web/app/_components/Checker.tsx
+++ apps/web/app/_components/Checker.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Checker() {
-    return (
-        <div>
-            Checker
-        </div>
-    )
+	return <div>Checker</div>;
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,6 @@
 import Checker from "@/app/_components/Checker";
 import React from "react";
 
-
 export default function page() {
 	return (
 		<div className="">
