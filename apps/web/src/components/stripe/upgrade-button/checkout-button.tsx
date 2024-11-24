22cc41b7e45568203c281f643ac2ca2d97e60e75
codersaadi <userid925925@gmail.com>
2024-11-25 03:08:04 +0500

    fix: added stripe button for server action

---
+++ apps/web/src/components/stripe/upgrade-button/checkout-button.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+
+import { generateStripeSessionAction } from '@/lib/stripe/actions'
+import { LoaderButton } from '@repo/ui/components/loader-button'
+import type { ReactNode } from 'react'
+import { useServerAction } from 'zsa-react'
+
+export function CheckoutButton({
+  className,
+  children,
+  priceId,
+}: {
+  className?: string
+  children: ReactNode
+  priceId: string
+}) {
+  const { execute, isPending } = useServerAction(generateStripeSessionAction)
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        execute({ priceId })
+      }}
+    >
+      <LoaderButton isLoading={isPending} className={className}>
+        {children}
+      </LoaderButton>
+    </form>
+  )
+}
