f9b20846732c57ee7e709e1b0f005c22b8269b7c
codersaadi <userid925925@gmail.com>
2024-11-25 03:13:01 +0500

    fix: minor fix

--- apps/web/src/components/stripe/upgrade-button/checkout-button.tsx
+++ apps/web/src/components/stripe/upgrade-button/checkout-button.tsx
@@ -1,32 +1,31 @@
-'use client'
+"use client";
 
-
-import { generateStripeSessionAction } from '@/lib/stripe/actions'
-import { LoaderButton } from '@repo/ui/components/loader-button'
-import type { ReactNode } from 'react'
-import { useServerAction } from 'zsa-react'
+import { generateStripeSessionAction } from "@/lib/stripe/actions";
+import { LoaderButton } from "@repo/ui/components/loader-button";
+import type { ReactNode } from "react";
+import { useServerAction } from "zsa-react";
 
 export function CheckoutButton({
-  className,
-  children,
-  priceId,
+	className,
+	children,
+	priceId,
 }: {
-  className?: string
-  children: ReactNode
-  priceId: string
+	className?: string;
+	children: ReactNode;
+	priceId: string;
 }) {
-  const { execute, isPending } = useServerAction(generateStripeSessionAction)
+	const { execute, isPending } = useServerAction(generateStripeSessionAction);
 
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-        execute({ priceId })
-      }}
-    >
-      <LoaderButton isLoading={isPending} className={className}>
-        {children}
-      </LoaderButton>
-    </form>
-  )
+	return (
+		<form
+			onSubmit={(e) => {
+				e.preventDefault();
+				execute({ priceId });
+			}}
+		>
+			<LoaderButton isLoading={isPending} className={className}>
+				{children}
+			</LoaderButton>
+		</form>
+	);
 }
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,19 +1,18 @@
-import env from "@repo/env";
 import { type PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 let db: PostgresJsDatabase<typeof schema>;
 let pg: ReturnType<typeof postgres>;
 
-if (env.NODE_ENV === "production") {
-  pg = postgres(env.DATABASE_URL || "");
+if (process.env.NODE_ENV === "production") {
+  pg = postgres(process.env.DATABASE_URL || "");
   db = drizzle(pg, { schema });
 } else {
   if (
     !(global as unknown as { database: PostgresJsDatabase<typeof schema> })
       .database
   ) {
-    pg = postgres(env.DATABASE_URL || "");
+    pg = postgres(process.env.DATABASE_URL || "");
     (
       global as unknown as { database: PostgresJsDatabase<typeof schema> }
     ).database = drizzle(pg, { schema });
