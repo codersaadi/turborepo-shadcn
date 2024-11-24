2c3a990b65cae7009d12767277019ca69f46ffc8
codersaadi <userid925925@gmail.com>
2024-11-24 11:34:56 +0500

    refactor: moved fonts to a seperate file - fonts.ts

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "@repo/ui/globals.css";
+import { geistMono, geistSans } from "./fonts";
 import RootProvider from "./root-provider";
 
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
