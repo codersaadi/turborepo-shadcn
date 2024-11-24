a1eefb0d52c6fa7c4a72246c80877148342532de
codersaadi <userid925925@gmail.com>
2024-11-24 12:20:26 +0500

    refactor: minor

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,21 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-leading-blank": [1, "always"],
-    "body-max-line-length": [2, "always", 100],
-    "footer-leading-blank": [1, "always"],
-    "footer-max-line-length": [2, "always", 100],
-    "header-max-length": [2, "always", 100],
-    "scope-case": [2, "always", "lower-case"],
+    "body-leading-blank": [1, "always"], // Ensure a blank line after the body
+    "body-max-line-length": [2, "always", 100], // Limit body line length to 100 characters
+    "footer-leading-blank": [1, "always"], // Ensure a blank line before the footer
+    "footer-max-line-length": [2, "always", 100], // Limit footer line length to 100 characters
+    "header-max-length": [2, "always", 100], // Limit header length to 100 characters
+    "scope-case": [2, "always", "lower-case"], // Scope must be in lower case
     "subject-case": [
       2,
       "never",
-      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+      ["sentence-case", "start-case", "pascal-case", "upper-case"], // Subject case rules
     ],
-    "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "type-case": [2, "always", "lower-case"],
-    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"], // Subject must not be empty
+    "subject-full-stop": [2, "never", "."], // Subject must not end with a period
+    "type-case": [2, "always", "lower-case"], // Type must be in lower case
+    "type-empty": [2, "never"], // Type must not be empty
     "type-enum": [
       2,
       "always",
@@ -31,7 +31,7 @@ module.exports = {
         "revert",
         "style",
         "test",
-      ],
+      ], // Allowed types for commits
     ],
   },
 };
