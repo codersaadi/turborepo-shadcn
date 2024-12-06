5378f7e7e4d00552e00ec3b8e413b3d7092879ab
codersaadi <userid925925@gmail.com>
2024-12-07 02:41:32 +0500

    fix: added a centralized trpc react client for react query, removed the duplicate

--- apps/web/src/lib/trpc/trpc-client-provider.tsx
+++ apps/web/src/lib/trpc/trpc-client-provider.tsx
@@ -1,21 +1,18 @@
 "use client";
-import type { AppRouter } from "@repo/api";
 import {
 	type QueryClient,
 	QueryClientProvider,
 	isServer,
 } from "@tanstack/react-query";
-import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 import { makeQueryClient } from "./query-client";
-import { trpcLinks } from "./react-client";
+import { trpcLinks, trpcReact } from "./react-client";
 export let browserQueryClient: QueryClient | undefined = undefined;
 function getQueryClient() {
 	if (isServer) return makeQueryClient();
 	if (!browserQueryClient) browserQueryClient = makeQueryClient();
 	return browserQueryClient;
 }
-export const trpcReact = createTRPCReact<AppRouter>({});
 
 export default function ReactQueryProvider({
 	children,
