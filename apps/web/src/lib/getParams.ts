a1e7a63aabede86e0e1952dd45fc20a1883daaa3
codersaadi <userid925925@gmail.com>
2024-12-08 05:03:31 +0500

    feat: added organization react components, trpc routers left

--- apps/web/src/lib/getParams.ts
+++ apps/web/src/lib/getParams.ts
@@ -3,23 +3,24 @@ import type { z } from "zod";
 export type SearchParams = Record<string, string | null | number>;
 type ParamsInput = Params | string | number | null;
 async function schemaResultAsync<T extends z.ZodTypeAny>(
-	data: Promise<unknown> | unknown,
-	schema: T,
+  data: Promise<unknown> | unknown,
+  schema: T
 ) {
-	return schema.parse(
-		data instanceof Promise ? await data : data,
-	) as Zod.infer<T>;
+  const parsedData = await (data instanceof Promise
+    ? data
+    : Promise.resolve(data));
+  return schema.parse(parsedData) as Zod.infer<T>;
 }
 
 export async function getParamsStrict<T extends z.ZodTypeAny>(
-	params: Promise<ParamsInput>,
-	schema: T,
+  params: Promise<ParamsInput>,
+  schema: T
 ) {
-	return schemaResultAsync(params, schema);
+  return schemaResultAsync(params, schema);
 }
 export async function getSearchParams<T extends z.AnyZodObject>(
-	searchParams: Promise<SearchParams>,
-	schema: T,
+  searchParams: Promise<SearchParams>,
+  schema: T
 ) {
-	return schemaResultAsync(searchParams, schema);
+  return schemaResultAsync(searchParams, schema);
 }
