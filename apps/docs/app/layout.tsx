1c331910cf96ffffbdad874c98c89e5ac49e40f0
codersaadi <userid925925@gmail.com>
2024-12-21 18:40:44 +0500

    fix: fixed docs tailwind issue and search endpoint

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body className="flex flex-col min-h-screen">
 				<RootProvider>{children}</RootProvider>
 			</body>
