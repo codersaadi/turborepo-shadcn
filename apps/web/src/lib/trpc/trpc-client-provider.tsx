7bbf4832a6ed05d88cab0470c3ab1aa02b154334
codersaadi <userid925925@gmail.com>
2025-03-07 13:48:09 +0500

    fix: major changes, trpc latest added

--- apps/web/src/lib/trpc/trpc-client-provider.tsx
+++ apps/web/src/lib/trpc/trpc-client-provider.tsx
@@ -1,12 +1,14 @@
 "use client";
+import type { AppRouter } from "@repo/api";
 import {
 	type QueryClient,
 	QueryClientProvider,
 	isServer,
 } from "@tanstack/react-query";
+import { createTRPCClient } from "@trpc/client";
 import { useState } from "react";
 import { makeQueryClient } from "./query-client";
-import { trpcLinks, trpcReact } from "./react-client";
+import { TRPCProvider, trpcLinks } from "./react-client";
 export let browserQueryClient: QueryClient | undefined = undefined;
 function getQueryClient() {
 	if (isServer) return makeQueryClient();
@@ -19,14 +21,16 @@ export default function ReactQueryProvider({
 }: { children: React.ReactNode }) {
 	const [queryClient] = useState(() => getQueryClient());
 	const [trpcClient] = useState(() => {
-		return trpcReact.createClient({
+		return createTRPCClient<AppRouter>({
 			links: trpcLinks,
 		});
 	});
 
 	return (
-		<trpcReact.Provider client={trpcClient} queryClient={queryClient}>
-			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-		</trpcReact.Provider>
+		<QueryClientProvider client={queryClient}>
+			<TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
+				{children}
+			</TRPCProvider>
+		</QueryClientProvider>
 	);
 }
