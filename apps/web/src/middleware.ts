4952cf361e30fdc0fa0281b584fe6420e1161073
codersaadi <userid925925@gmail.com>
2025-04-03 01:20:44 +0500

    fix: fixed configurations

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,26 +1,27 @@
 import { createAuthMiddleware } from "@authjs/core/middleware";
+import type { AppRouteHandlerFn } from "next/dist/server/route-modules/app-route/module";
 import {
-	DEFAULT_LOGIN_REDIRECT,
-	SIGNIN_PAGE,
-	apiAuthPrefix,
-	authRoutes,
-	publicRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  SIGNIN_PAGE,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
 } from "./routes";
 
 const middleware = createAuthMiddleware({
-	publicRoutes,
-	authRoutes,
-	apiAuthPrefix,
-	loginRedirect: DEFAULT_LOGIN_REDIRECT,
-	signin: SIGNIN_PAGE,
-});
+  publicRoutes,
+  authRoutes,
+  apiAuthPrefix,
+  loginRedirect: DEFAULT_LOGIN_REDIRECT,
+  signin: SIGNIN_PAGE,
+}) as AppRouteHandlerFn;
 export default middleware;
 
 export const config = {
-	matcher: [
-		// Skip Next.js internals and all static files, unless found in search params
-		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-		// Always run for API routes
-		"/(api|trpc)(.*)",
-	],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
